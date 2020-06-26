ffbf889f6f02a3ed538c866fca49fbd0af3f0c68
donut0711 <markers711@gmail.com>
2020-06-26 16:24:33 -0400

    added swallow patch

--- config.h
+++ config.h
@@ -3,6 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pix*/
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const unsigned int gappx     = 5; 
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -27,9 +28,7 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	/*{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },*/
+	{ "st",      NULL,     NULL,           0,         0,          1,          -1,        -1 },
 };
 
 /* layout(s) */
@@ -59,15 +58,12 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *duck[] = {"surf", "duck.com"};
-static const char *archwiki[] = {"surf", "wiki.archlinux.org"};
-static const char *chan[] = {"surf", "4chan.org"};
-
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_o,      spawn,          SHCMD("bash ~/scripts/quick.sh") },
+	{ MODKEY,                       XK_n,      spawn,          SHCMD("st -e newsboat") },
 	{ MODKEY/*|ShiftMask*/,         XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -90,9 +86,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY|ShiftMask,		XK_d,	   spawn,	   {.v =  duck } },
-	{ MODKEY|ShiftMask,		XK_a,	   spawn,	   {.v =  archwiki } },
-	{ MODKEY|ShiftMask,		XK_c,	   spawn,	   {.v =  chan } },
+	{ MODKEY|ShiftMask,		XK_d,	   spawn,	   SHCMD("surf duckduckgo.com" ) },
+	{ MODKEY|ShiftMask,		XK_a,	   spawn,	   SHCMD("surf wiki.archlinux.org") },
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
